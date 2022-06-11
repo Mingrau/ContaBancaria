3021e1c9a75d9bebd0915ef8e8e28672e2de40fe
mingrau <alexandreramoos11@gmail.com>
2022-06-11 15:54:46 -0300

    Adicionando Arquivos do Exercicio

---
+++ ExercicioContaBancaria/Conta.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioContaBancaria
+{
+     class Conta
+    {
+        public string Titular { get; set; }
+        public int Numero { get; set; }        
+        public double Saldo { get; set; }
+
+
+        public Conta()
+        {
+
+        }
+
+        public Conta(string titular, int numero)
+        {
+            Titular = titular;
+            Numero = numero;
+        }
+
+        public Conta(string titular, int numero, double saldo)
+        {
+            Titular = titular;
+            Numero = numero;
+            Saldo = saldo;
+        }
+
+        public double AdicionarlSaldo(double saldo) {
+
+            Saldo = Saldo + saldo;
+            return Saldo;
+        }
+
+        public double Sacar(double saldo) { 
+            
+            return Saldo =  (Saldo - saldo) - 5;
+        }
+
+        public override string ToString()
+        {
+            return  "Numero da Conta: " + Numero + "\nTitular da Conta: " + Titular + "\nSaldo Atual: $" + Saldo;
+
+        }
+    }
+}
---
+++ ExercicioContaBancaria/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioContaBancaria
+{
+    class Progam
+    {
+        static void Main(string[] args)
+        {   string escolha;
+            Conta p = new Conta();
+            
+            Console.WriteLine("-----CONTA BANCARIA-----");
+            Console.WriteLine();
+            Console.Write("Digite o numero da conta: ");
+            p.Numero = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o titular da conta: ");
+            p.Titular = Console.ReadLine();
+
+            Console.Write("Deseja fazer um deposito s/n : ");
+            escolha = Console.ReadLine();
+            if (escolha == "s")
+            {
+                Console.Write("Digite o valor a ser aplicado: $");
+                p.Saldo = double.Parse(Console.ReadLine());
+                Console.WriteLine();
+            }
+            Console.WriteLine($"Dados da Conta: {p.Titular}");
+            Console.WriteLine(p);
+            Console.WriteLine();
+
+            Console.Write("Entre com um valor para deposito: $");
+            double novoDeposito = double.Parse(Console.ReadLine());
+            Console.WriteLine();
+            p.AdicionarlSaldo(novoDeposito);
+
+            Console.WriteLine("Dados da Conta: ");
+            Console.WriteLine(p);
+
+            Console.Write("Insira um valor para saque (*taxa administrativa de $5,00) :");
+            double sacar = double.Parse(Console.ReadLine());
+            p.Sacar(sacar);
+            Console.WriteLine();
+
+            Console.WriteLine(p);
+
+
+        }
+    }
+}
